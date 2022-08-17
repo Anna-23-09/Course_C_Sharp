37164df2ef76f5ad75ce523a285788796628594b
Anna <tolstovaaniuta@yandex.ru>
2022-08-17 09:54:31 +0300

    3 hw

--- 7_lesson/HW/HW_3/Program.cs
+++ 7_lesson/HW/HW_3/Program.cs
@@ -5,9 +5,9 @@ void Print(int[,] arr)
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
 
-    for(int i = 0; i < row_size; i++)
+    for (int i = 0; i < row_size; i++)
     {
-        for(int j = 0; j < column_size; j++)
+        for (int j = 0; j < column_size; j++)
         {
             Console.Write($" {arr[i, j]} ");
         }
@@ -20,14 +20,33 @@ int[,] MassNums(int row, int column, int from, int to)
 {
     int[,] arr = new int[row, column];
 
-    for(int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j = 0; j < column; j++)
+        for (int j = 0; j < column; j++)
         {
             arr[i, j] = new Random().Next(from, to);
-        }        
+        }
     }
-    return arr;    
+    return arr;
+}
+
+
+
+void Average(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    double result;
+
+    for (int i = 0; i < column; i++)
+    {
+        result = 0;
+        for (int j = 0; j < row; j++)
+        {
+            result += arr[j, i];
+            Console.Write($"{Math.Round(result / row, 2)}; ");
+        }
+    }
 }
 
 Console.Write("Enter the number of rows: ");
@@ -35,23 +54,10 @@ int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
 
-int Average (int [,] arr, int sum_num)
-{   
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {   
-        int ave_arr = 0;
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            ave_arr = arr [i, j];
-            j++;
-            sum_num = arr [i, j];
-        }
-    }
-}
-
 int[,] arr_1 = MassNums(row, column, 1, 11);
+Print(arr_1);
 
-Print (arr_1);
-Average ();
+Average(arr_1);
+
 
 
