4505c6bee5dc8ef2fad7b728cfe7c8d339069b3d
Anna <tolstovaaniuta@yandex.ru>
2022-08-19 12:09:05 +0300

    hw 2

--- 8_lesson/HW/HW_2/Program.cs
+++ 8_lesson/HW/HW_2/Program.cs
@@ -1,15 +1,16 @@
 ﻿// Задайте прямоугольный двумерный массив. 
-// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Напишите программу, которая будет находить строку с 
+//наименьшей суммой элементов.
 
-void Print(int[,] array) 
+void Print(int[,] arr) 
 { 
-    int row_size = array.GetLength(0); 
-    int column_size = array.GetLength(1); 
+    int row_size = arr.GetLength(0); 
+    int column_size = arr.GetLength(1); 
     for (int i = 0; i < row_size; i++) 
     { 
         for (int j = 0; j < column_size; j++) 
         { 
-            Console.Write($" {array[i, j]} "); 
+            Console.Write($" {arr[i, j]} "); 
         } 
         Console.WriteLine(); 
     } 
@@ -18,40 +19,41 @@ void Print(int[,] array)
 
 int [,] MassNums(int row, int column, int from , int  to) 
 { 
-    int[,] array = new int [row, column]; 
+    int[,] arr = new int [row, column]; 
  
     for (int i = 0; i < row; i++) 
     { 
         for (int j = 0; j < column; j++) 
         { 
-            array[i, j] = new Random().Next(from,to); 
+            arr[i, j] = new Random().Next(from,to); 
         }  
     } 
-    return array; 
+    return arr; 
 } 
 
-int [,] FindMinRow (int [,] arr)
+void RowSmallAmount(int[,] arr)
 {
-    int row = arr.GetLength(0);
-    int column = arr.GetLength(1);
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int amount = 1000;
+    int num;
+    int smallest = 0;
 
-    int sum = 0;
-    int index = 0;
+    for (int i = 0; i < row_size; i++)
+    {   
+        num = 0;
+        for (int j = 0; j < column_size; j++)
+            num += arr[i, j];
+            Console.Write($"{num, 4} ");            
 
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        int max_sum = 0;
-        for (int j = 0; j < arr.GetLength(1); j++)
+        if (amount > num)
         {
-            sum += arr[i, j];
-        }
-        if (sum > max_sum)
-        {
-            sum = max_sum;
-            index = i++;
+            amount = num;
+            smallest = i;
         }
     }
-    Console.WriteLine($"{arr} [i] [j]");
+    Console.WriteLine();
+    Console.Write($"The row with the lowest amount per position - {smallest + 1}");
 }
 
 Console.Write("Enter the number of rows: ");
@@ -59,8 +61,7 @@ int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
 
-int[,] arr_1 = MassNums(row, column, 1, 10);
+int[,] arr_1 = MassNums(row, column, 0, 10);
 Print(arr_1);
 
-FindMinRow(arr_1);
-Print(arr_1);
+RowSmallAmount(arr_1);
