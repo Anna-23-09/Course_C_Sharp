8045f4635404d7e199da5b76c28e450d0df1b3d2
Anna <tolstovaaniuta@yandex.ru>
2022-08-19 11:57:11 +0300

    hw 1

---
+++ 9_lesson/HW/HW_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+int NaturalNumbers (int a, int b)
+{
+    if (a % 2 == 0) return a;
+    return NaturalNumbers(a, b - 1);
+    Console.WriteLine($"{a + 2}, ");
+}
+Console.WriteLine(NaturalNumbers(15, 29));
